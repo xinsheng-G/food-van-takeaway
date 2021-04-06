39748a36524a1d834fa2c4ddbfbfbd5640a855bf
xiaotian <xiaotian.li5@student.unimelb.edu.au>
2021-04-06 16:05:34 +0800

    add Procfile for Heroku

--- utils/MD5_utils.js
+++ utils/MD5_utils.js
@@ -1,6 +1,8 @@
 const crypto = require("crypto");
 
 // encrypt the password
+// MD5 encryption can be breached by Rainbow Tables，we can
+// change it to a more secure encryption algorithm later
 function encrypt(password) {
     let md5 = crypto.createHash("md5");
     return md5.update(password).digest('hex');
