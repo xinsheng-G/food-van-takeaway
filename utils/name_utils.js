d1df6227641604665ffd44b97a908800701ff744
Blackmesa-Canteen <shaotienlee@foxmail.com>
2021-04-20 23:49:04 +0800

    1. add search_page_base 2. add van_detail_page_base 3. add checkout_page_base

--- utils/name_utils.js
+++ utils/name_utils.js
@@ -1,6 +1,5 @@
 // Replace the underscore in the string with a space and change the initial letter to uppercase
-// useful to handle
-// change it to a more secure encryption algorithm later
+// useful to handle object name differences between database and html page
 function change_dash_into_space(name) {
     let res = name.replace(/_/g, ' ');
     return res.charAt(0).toUpperCase() + res.slice(1);
